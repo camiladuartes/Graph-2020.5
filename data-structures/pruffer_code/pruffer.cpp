1b0a47e7dae3d56c728d1ae620ec5de323a07f72
Marcos Delgado <marcosdelgado0408@gmail.com>
2020-06-23 14:35:27 -0300

    Adicionando algoritmos para criar vetor de graus de vertices e decodificar o código pruffer

--- data-structures/pruffer_code/pruffer.cpp
+++ data-structures/pruffer_code/pruffer.cpp
@@ -6,11 +6,13 @@ using namespace std;
 class Pruffer{
     private:
         vector<int> code;
+        vector<int> nodeDegrees;
+
     public:
 
-        const vector<int> &getCode() const {
-            return code;
-        }
+        const vector<int> &getCode() const { return code;}
+        const vector<int> &getNodeDegrees() const { return nodeDegrees;}
+
 
     //  will check if the vertex is a leaf
         bool isLeaf(Graph graph, int vertexVal){
@@ -26,12 +28,11 @@ class Pruffer{
         }
 
 
+
         void removeLeaf(Graph *graph){
             // verificar em todos os vertices se ele sao leafs ou n
             for(int i=0;i<graph->getAmountVert();i++){
                 if(isLeaf(*graph, i)){
-                    cout << i << " is leaf" << endl;
-                    cout <<"adjacente ao " << i << " : " << graph->getAdjacentNodes(i) << endl;
                     this->code.push_back(graph->getAdjacentNodes(i)[0]); // if its a leaf node, it has only 1 adjacent node and it's in position 0
                     graph->removeEdge(i, graph->getAdjacentNodes(i)[0]);
                     break;
@@ -39,30 +40,104 @@ class Pruffer{
             }
         }
 
+
+
         // this process will be repeated n-2 times
         void encode(Graph *graph){
             for(int i=0;i<graph->getAmountVert()-2;i++){
                 removeLeaf(graph);
-                graph->printMatrix();
             }
         }
+
+
+
+        void findNodeDegrees(){
+            auto nodeDegreesSize = this->code.size() + 2; // the size os this list will be the code size + 2
+            auto counter = 0;
+
+            for(int i=0;i<nodeDegreesSize;i++){
+                counter = 0;
+                for(int j=0;j<code.size();j++){
+                    if(code[j] == i){
+                        counter++;
+                    }
+                }
+                nodeDegrees.push_back(counter + 1); // the degree will be the number of times i can find [i] in the code list + 1
+            }
+        }
+
+
+
+        // this method will find the vortex with the minor label with degree 1
+        int findMinorLabel(vector<int> nodeDegrees){
+            for(int i=0;i<nodeDegrees.size();i++){
+                if(nodeDegrees[i] == 1){
+                    return i;
+                }
+            }
+        }
+
+
+
+        void decode(Graph *graph){
+            int vortex1;
+            int vortex2;
+            vector<int> lastVortexes;
+
+            // here i'll have to take a number from puffer code and then increment in the vector
+            // in the nodeDegrees vector i'll have to find a degree 1 vertex with the smallest label
+            for(int i=0;i<this->code.size();i++){
+                vortex1 = code[i];
+                vortex2 = findMinorLabel(this->nodeDegrees);
+
+                nodeDegrees[vortex1]--;
+                nodeDegrees[vortex2]--;
+
+                graph->addEdge(vortex1, vortex2);
+            }
+
+            // after go through all the code vector it will last two indexes with degree 1
+            for(int i=0;i<nodeDegrees.size();i++){
+                if(nodeDegrees[i] == 1){
+                    lastVortexes.push_back(i);
+                    nodeDegrees[i]--;
+                }
+            }
+            graph->addEdge(lastVortexes[0], lastVortexes[1]);
+
+        }
+
 
 
 };
 
 
 int main(){
-    Graph *graph = new Graph(5);
+
+    Graph *graph = new Graph(8);
     graph->addEdge(0, 1);
-    graph->addEdge(0, 2);
-    graph->addEdge(2, 3);
-    graph->addEdge(3, 4);
+    graph->addEdge(1, 2);
+    graph->addEdge(1, 3);
+    graph->addEdge(3, 6);
+    graph->addEdge(2, 5);
+    graph->addEdge(2, 4);
+    graph->addEdge(4, 7);
     graph->printMatrix();
 
+    cout << endl << endl;
+
     Pruffer *pruffer = new Pruffer();
-
     pruffer->encode(graph);
+    graph->printMatrix();
 
-    cout << pruffer->getCode();
+    cout << "Pruffer code vector: " << pruffer->getCode() << endl;
+    pruffer->findNodeDegrees();
+    cout << "Node degrees vector: " << pruffer->getNodeDegrees() << endl << endl;
+
+    pruffer->decode(graph);
+
+    graph->printMatrix();
+
+
     return 0;
 }
